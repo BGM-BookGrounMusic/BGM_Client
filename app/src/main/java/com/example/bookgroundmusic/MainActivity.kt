84a55e7ae758c75a0ebbb4eb4ab150d1f075ef90
wonjinyoo <splendoes@naver.com>
2023-02-23 16:43:43 +0900

    원진 UI 일부 update (Scrollview)

--- app/src/main/java/com/example/bookgroundmusic/MainActivity.kt
+++ app/src/main/java/com/example/bookgroundmusic/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.bookgroundmusic
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 아래 코드처럼 모드나 음악 설정 시 Textview 클릭 시 글자색 바뀌는 걸로 하고, 실질적으로 서버에는 boolean 값 넘겨주면 될 것 같음
 
+//        val mode_1 = findViewById<TextView>(R.id.mode_1)
+//        var only_bgm = false
+//        mode_1.setOnClickListener(View.OnClickListener {
+//            if (!only_bgm) {
+//                mode_1.setTextColor(Color.parseColor("#FF730D"))
+//                only_bgm = true
+//            } else {
+//                mode_1.setTextColor(Color.parseColor("#000000"))
+//                only_bgm = false
+//            }
+//        })
     }
 }
