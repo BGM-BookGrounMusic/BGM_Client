7c2196bdac7207975abcb3d7b90a8a423f32f9f7
wonjinyoo <splendoes@naver.com>
2023-03-05 15:47:09 +0900

    원진 스플래시 화면 & 사용설명서 화면 update

--- app/src/main/java/com/example/bookgroundmusic/GuideActivity.kt
+++ app/src/main/java/com/example/bookgroundmusic/GuideActivity.kt
@@ -1,14 +1,25 @@
 package com.example.bookgroundmusic
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_guide.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-// 사용 설명서 화면용
+// 사용 설명서 화면
 
 class GuideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guide)
+
+
+        // 메인 화면으로 이동
+        btn_backtomain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
