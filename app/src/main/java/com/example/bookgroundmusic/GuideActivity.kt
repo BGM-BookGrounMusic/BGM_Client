57fa76455f2c883d2de644350a69ae95f59a208d
wonjinyoo <splendoes@naver.com>
2023-03-24 16:11:29 +0900

    [원진] 노래 재생 시도중 (Storage)

--- app/src/main/java/com/example/bookgroundmusic/GuideActivity.kt
+++ app/src/main/java/com/example/bookgroundmusic/GuideActivity.kt
@@ -3,20 +3,23 @@ package com.example.bookgroundmusic
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_guide.*
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.bookgroundmusic.databinding.ActivityGuideBinding
 
 // 사용 설명서 화면
 
 class GuideActivity : AppCompatActivity() {
+    private var mBinding: ActivityGuideBinding? = null
+    private val binding get() = mBinding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guide)
 
+        mBinding = ActivityGuideBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // 메인 화면으로 이동
-        btn_backtomain.setOnClickListener {
+        binding.btnBacktomain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/bookgroundmusic/MainActivity.kt
+++ app/src/main/java/com/example/bookgroundmusic/MainActivity.kt
@@ -2,41 +2,44 @@ package com.example.bookgroundmusic
 
 import android.R
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bookgroundmusic.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.storage.FirebaseStorage
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
-import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 import java.io.*
 import java.util.concurrent.TimeUnit
+import org.json.JSONArray
+import kotlin.random.Random
+import java.io.FileInputStream
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.streams.toList
 
 
 class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
-    var dir: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        // firebase 연동 관련 코드
-//        val serviceAccount = FileInputStream("app/src/main/assets/backgroundmusic-949f0-firebase-adminsdk-1xdu2-d57d4620e7.json")
-//
-//        val options: FirebaseOptions = FirebaseOptions.Builder()
-//            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//            .setDatabaseUrl("https://backgroundmusic-949f0-default-rtdb.firebaseio.com")
-//            .build()
-//
-//        FirebaseApp.initializeApp(options)
 
         //permissionCheck()
         startMainService()
@@ -60,11 +63,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    // Root : 슈퍼유저 권한 주기
-    var p: Process? = null
-    init {
-        var p = java.lang.Runtime.getRuntime().exec("su")
-    }
+//    // Root : 슈퍼유저 권한 주기
+//    var p: Process? = null
+//
+//    init {
+//        var p = java.lang.Runtime.getRuntime().exec("su")
+//    }
 
     override fun onDestroy() {
         super.onDestroy()
@@ -98,10 +102,27 @@ class MainActivity : AppCompatActivity() {
         stopService(serviceIntent)
     }
 
+    // 음악 재생하도록
+    private fun playMusic() {
+        Toast.makeText(this, "버튼 클릭됨", Toast.LENGTH_LONG).show()
+
+        val storage = FirebaseStorage.getInstance()
+        val storageReference = storage.getReference()
+
+        val pathReference = storageReference.child("감성음악/기쁨/001.mp3")
+
+        // test
+        val mediaPlayer : MediaPlayer? = MediaPlayer().apply {
+            setDataSource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+            prepare()
+            start()
+        }
+    }
+
 
     private fun setListener() {
         // 1. on 버튼 클릭시
-        btn_on.setOnClickListener {
+        binding.btnOn.setOnClickListener {
             Toast.makeText(this, "5초 후 캡처 시작", Toast.LENGTH_LONG).show()
 
             try {
@@ -110,22 +131,76 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
 
+            // 백그라운드 캡처
+            MediaProjectionController.screenCapture(this@MainActivity) { bitmap ->
+                // TODO : You can use the captured image (bitmap)
 
+                // OCR (ML Kit)
+                val options = KoreanTextRecognizerOptions.Builder().build()
+                val recognizer = TextRecognition.getClient(options)
+                val image = InputImage.fromBitmap(bitmap, 0)
 
+                val result = recognizer.process(image)
+                    .addOnSuccessListener { visionText ->
+                        // string to txt file
+                        val fileName = "text.txt"
+
+                        var outputFile: FileOutputStream = openFileOutput(fileName, MODE_PRIVATE)
+                        outputFile.write(visionText.text.toByteArray())
+                        outputFile.close()
+                    }
+                    .addOnFailureListener { e -> }
+
+//
+//                // 보영 코드
+//                // firebase 연동 관련 코드
+//                val serviceAccount =
+//                    FileInputStream("app/src/main/assets/backgroundmusic-949f0-firebase-adminsdk-1xdu2-d57d4620e7.json")
+//
+//                val option: FirebaseOptions = FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .setDatabaseUrl("https://backgroundmusic-949f0-default-rtdb.firebaseio.com")
+//                    .setStorageBucket("backgroundmusic-949f0.appspot.com")
+//                    .build()
+//
+//                FirebaseApp.initializeApp(option)
+//
+//                //val database: DatabaseReference
+//                val bucket = FirebaseStorage.getInstance()
+//
+//
+//                val database = FirebaseDatabase.getInstance().getReference()
+//
+//
+//
+//                fun findEmotion(): Query {
+//                    val prediction = "슬픔" // 이것은 nlp모델의 결과로 얻은(predict) 값이다.
+//                    val mode = "감성음악/"
+//                    val modePath = "$mode$prediction"
+//
+//                    // Get a reference to the Firebase Realtime Database node for 감성음악/슬픔
+//                    val emotionalRef = database.ref
+//
+//                    val emotionalQuery = emotionalRef.orderByKey().limitToFirst(2) // 노래가져오는 갯수
+////                    val emotionalData = emotionalQuery.
+////                    val playlistEmo = emotionalData.values().shuffled().take()
+//
+//                    return emotionalQuery
+//
+//                }
 
 
 //                MainApplication.updateNotification(this, "스크린샷")
 //                Toast.makeText(this, "스크린캡처 테스트", Toast.LENGTH_SHORT).show()
 
+            }
 
-
+            // 2. 사용 설명서 화면으로 이동
+            binding.buttonGuide.setOnClickListener {
+                val intent = Intent(this, GuideActivity::class.java)
+                startActivity(intent)
+            }
         }
 
-        // 2. 사용 설명서 화면으로 이동
-        button_guide.setOnClickListener {
-            val intent = Intent(this, GuideActivity::class.java)
-            startActivity(intent)
-        }
     }
-
 }
